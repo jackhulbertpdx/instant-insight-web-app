d381f7378219df805085a5fee202fe118ba6fbc8
jackhulbertpdx <39444980+jackhulbertpdx@users.noreply.github.com>
2024-04-28 04:27:22 +0000

    commit

--- main.py
+++ main.py
@@ -3,16 +3,6 @@
 import streamlit as st
 import os
 
-# Everything is accessible via the st.secrets dict:
-st.write("user:", st.secrets["user"])
-st.write("password:", st.secrets["password"])
-st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
-
-# And the root-level secrets are also accessible as environment variables:
-st.write(
-    "Has environment variables been set:",
-    os.environ["user"] == st.secrets["user"],
-)
 
 
 # Initialize connection.
