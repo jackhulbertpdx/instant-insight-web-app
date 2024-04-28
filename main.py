050864ebf297e929c255ace0ef624dce00d0a451
jackhulbertpdx <39444980+jackhulbertpdx@users.noreply.github.com>
2024-04-28 04:25:46 +0000

    commmit

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 # streamlit_app.py
 
 import streamlit as st
+import os
+
+# Everything is accessible via the st.secrets dict:
+st.write("user:", st.secrets["user"])
+st.write("password:", st.secrets["password"])
+st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+
+# And the root-level secrets are also accessible as environment variables:
+st.write(
+    "Has environment variables been set:",
+    os.environ["user"] == st.secrets["user"],
+)
+
 
 # Initialize connection.
 conn = st.connection("snowflake")
@@ -11,4 +24,3 @@ df = conn.query("SELECT * from STG_POOLS;", ttl=600)
 # Print results.
 for row in df.itertuples():
     st.write(f"{row.POOL_NAME} has a :{row.POOL_NAME}:")
-    
